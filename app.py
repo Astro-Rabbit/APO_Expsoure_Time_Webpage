243fc7a6e36e7158ce0955df4c494cff92aa4487
Alexander (Desktop) <stonemaa@nmsu.edu>
2020-03-20 19:03:09 -0600

    changed input.html to include drop down menu and radio button for plot type

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, request, render_template, Response
 from APOExptime import Sky, Target, Instrument, Observation, makeplots
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import random
 import io
 
 app = Flask(__name__)
@@ -18,16 +14,22 @@ def get_parameters():
 def my_form_post():
     instrument_name = str(request.args.get('instrument', 'World'))
     mag = float(request.args['mag'])
+    type = int(request.args['type'])
     temp = float(request.args['temp'])
     seeing = float(request.args['seeing'])
-    SN = int(request.args['SN'])
+    cacluation = int(request.args['cacluation'])
 
     inst = Instrument(instrument_name)
     sky = Sky(seeing=seeing)
     star = Target(mag, 'VEGAMAG', [4700,6900], temp=temp)
     ob = Observation(star, sky, inst)
-    ob.TimefromSN(SN)
-    fig = makeplots(ob, 'Time')
+    if type == 0:
+        ob.TimefromSN(cacluation)
+        type = 'Time'
+    elif type == 1:
+        ob.SNfromTime(cacluation)
+        type = 'SN'
+    fig = makeplots(ob, type)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
